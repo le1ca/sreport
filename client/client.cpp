e096cdec7dc8ef3b41c1605cfadb425bf19eb448
lq <lq@le1.ca>
2015-01-25 14:51:37 -0700

    use Jsonifier in main() instead of manual printing

--- client/client.cpp
+++ client/client.cpp
@@ -1,4 +1,5 @@
 #include "LibsensorManager.h"
+#include "Jsonifier.h"
 #include <string>
 #include <iostream>
 #include <vector>
@@ -12,15 +13,42 @@ int main(int argc, char** argv){
     LibsensorManager s = LibsensorManager();
     vector<AbstractSensor*> sensors = s.getSensors();
     
+    Jsonifier ja;
+    Jsonifier *js = new Jsonifier[sensors.size()];
+
+    JsonArray a;
+    a.len = sensors.size();
+    a.t   = (JsonEntry*) malloc(a.len * sizeof(JsonEntry));
+    
     for (unsigned i=0; i<sensors.size(); i++){
     	AbstractSensor* id = sensors.at(i);
+    	
+    	/*
     	cout << " {  " << endl;
     	cout << "    " << "name:  " << id->getName()  << endl;
     	cout << "    " << "group: " << id->getGroup() << endl;
     	cout << "    " << "label: " << id->getLabel() << endl;
     	cout << "    " << "value: " << id->getValue() << endl;
-		cout << " }  " << endl << endl;;
+		cout << " }  " << endl << endl;
+		*/
+
+		js[i].add_string("name",  id->getName() );
+		js[i].add_string("group", id->getGroup());
+		js[i].add_string("label", id->getLabel());
+		js[i].add_float ("value", id->getValue());
+		
+		JsonEntry e;
+		e.t = JSON_OBJECT;
+		e.d.o = &js[i];
+		a.t[i] = e;
 	}
+	
+	ja.add_array("sensors", a.t, a.len);
+	ja.print();
+	cout << endl;
+	
+	delete[] js;
+	free(a.t);
 	
     return 0;
 }
