791e2afb31713f4c295375528654846455039cb5
lq <lq@le1.ca>
2015-01-25 14:50:54 -0700

    implement basic Jsonifier functions

--- client/Jsonifier.cpp
+++ client/Jsonifier.cpp
@@ -4,12 +4,104 @@ Jsonifier::Jsonifier(){
 	
 }
 
-void Jsonifier::add(std::string k, std::string v){
-	(void) k; // to satisfy -Wpedantic for now
-	(void) v;
-	// TODO
+Jsonifier::~Jsonifier(){
+	std::map<std::string, JsonEntry>::iterator it;
+	
+	for(it = vals.begin(); it != vals.end(); it++){
+		JsonEntry v = it->second;
+		switch(v.t){
+			case JSON_STRING:
+				free((void*) v.d.s);
+				break;
+			default:
+				break;
+		}
+	}
+}
+
+void Jsonifier::add_string(std::string k, std::string v){
+	JsonEntry e;
+	e.t     = JSON_STRING;
+	e.d.s   = strdup(v.c_str());
+	vals[k] = e;
+}
+
+void Jsonifier::add_float(std::string k, double v){
+	JsonEntry e;
+	e.t     = JSON_FLOAT;
+	e.d.f   = v;
+	vals[k] = e;
+}
+
+void Jsonifier::add_array(std::string k, JsonEntry *v, int len){
+	JsonEntry e;
+	e.t       = JSON_ARRAY;
+	e.d.a.len = len;
+	e.d.a.t   = v;
+	vals[k]   = e;
+}
+
+void Jsonifier::print_entry(JsonEntry *e){
+	switch(e->t){
+		
+		case JSON_STRING: {
+			std::cout << "'" << e->d.s << "'";
+			break;
+		}
+		
+		case JSON_FLOAT:{
+			std::cout << e->d.f;
+			break;
+		}
+		
+		case JSON_INTEGER: {
+			std::cout << e->d.i;
+			break;
+		}
+				
+		case JSON_ARRAY: {
+			JsonEntry *h = e->d.a.t;
+			std::cout << "[";
+			for(int i = 0; i < e->d.a.len; i++){
+				print_entry(h+i);
+				if(i < e->d.a.len - 1)
+					std::cout << ",";
+			}
+			std::cout << "]";
+			break;
+		}
+				
+		case JSON_OBJECT: {
+			e->d.o->print();
+			break;
+		}
+				
+		default: {
+			throw std::runtime_error(
+				std::string("JSON printing for data type ") +
+				std::to_string((long) e->t) +
+				std::string(" not implemented")
+			);
+		}
+			
+	}
 }
 
 void Jsonifier::print(){
-	// TODO
+	std::map<std::string, JsonEntry>::iterator it = vals.begin();
+	std::cout << "{";
+	
+	while(it != vals.end()){
+		std::string k = it->first;
+		JsonEntry   v = it->second;
+		
+		std::cout << "'" << k << "':";
+		print_entry(&v);
+		
+		if(++it != vals.end()){
+			std::cout << ",";
+		}
+	}
+	
+	std::cout << "}";
 }
--- client/Jsonifier.h
+++ client/Jsonifier.h
@@ -1,32 +1,51 @@
 #ifndef Jsonifier_h
 #define Jsonifier_h
 #include <string>
+#include <iostream>
+#include <stdexcept>
+#include <cstring>
 #include <map>
+
+typedef struct JsonEntry JsonEntry;
 
 class Jsonifier {
   private:
-    std::map<std::string,std::string> vals;
+    std::map<std::string, JsonEntry> vals;
+    void print_entry(JsonEntry *e);
   public:
     Jsonifier();
-    void add(std::string k, std::string v);
+    ~Jsonifier();
+    void add_string(std::string k, std::string v);
+    void add_array(std::string k, JsonEntry *v, int len);
+    void add_float(std::string k, double v);
+    // TODO: add methods for other types
 	void print();
 };
 
 typedef enum JsonEntryType {
 	JSON_OBJECT,
 	JSON_STRING,
-	JSON_NUMBER,
+	JSON_INTEGER,
+	JSON_FLOAT,
 	JSON_ARRAY
 } JsonEntryType;
 
+typedef struct JsonEntry JsonEntry;
+
+typedef struct JsonArray {
+	int len;
+	JsonEntry *t;
+} JsonArray;
+
 typedef struct JsonEntry {
 	JsonEntryType t;
-	union d {
-		Jsonifier   o;
-		std::string s;
-		double      n; // TODO: make more generic to separate ints and floats
-		Jsonifier   a; // TODO: implement separate JSON array type
-	};
+	union {
+		Jsonifier   *o;
+		const char  *s;
+		long        i;
+		double      f;
+		JsonArray   a;
+	} d;
 } JsonEntry;
 
 #endif
