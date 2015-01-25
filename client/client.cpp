c146055f42f98cef2b56192ca7eb1827270665b1
lq <lq@le1.ca>
2015-01-25 13:27:25 -0700

    enumerate more data in test main() function

--- client/client.cpp
+++ client/client.cpp
@@ -6,11 +6,21 @@
 using namespace std;
 
 int main(int argc, char** argv){
+	(void) argc; // to satisfy -Wpedantic for now
+	(void) argv;
+
     LibsensorManager s = LibsensorManager();
     vector<AbstractSensor*> sensors = s.getSensors();
+    
     for (unsigned i=0; i<sensors.size(); i++){
     	AbstractSensor* id = sensors.at(i);
-		cout << id->getLabel() << " => " << id->getValue() << endl;
+    	cout << " {  " << endl;
+    	cout << "    " << "name:  " << id->getName()  << endl;
+    	cout << "    " << "group: " << id->getGroup() << endl;
+    	cout << "    " << "label: " << id->getLabel() << endl;
+    	cout << "    " << "value: " << id->getValue() << endl;
+		cout << " }  " << endl << endl;;
 	}
+	
     return 0;
 }
